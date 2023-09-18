4fb87dfa4be59caf32ef1a9d27093b8d8c802953
Krivoshey <krivoshey@logema.org>
2023-09-18 22:34:41 +0300

    [master] Разработка Ulid

---
+++ symfony/src/Shared/Domain/ValueObject/Ulid.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Shared\Domain\ValueObject;
+
+use DomainException;
+use Symfony\Component\Uid\Ulid as SymfonyUlid;
+
+readonly class Ulid
+{
+	private function __construct(
+		public string $value
+	) {}
+
+	public static function generate(): Ulid
+	{
+		return new self(SymfonyUlid::generate());
+	}
+
+	public static function fromString(string $value): Ulid
+	{
+		if (!self::isValid($value)) {
+			throw new DomainException('Invalid Ulid Value');
+		}
+
+		return new self($value);
+	}
+
+	public static function isValid(string $value): bool
+	{
+		return SymfonyUlid::isValid($value);
+	}
+}
