cda7d5849fc0b3825d2d4e8ddc5bdd77467910aa
Krivoshey <krivoshey@logema.org>
2023-09-18 22:35:13 +0300

    [master] Установил codeception

---
+++ symfony/tests/unit/Shared/UlidTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace unit\Shared;
+
+use App\Shared\Domain\ValueObject\Ulid;
+use App\Tests\UnitTester;
+use Codeception\Test\Unit;
+use Symfony\Component\Uid\Ulid as SymfonyUlid;
+
+class UlidTest extends Unit
+{
+	protected UnitTester $tester;
+
+	protected function _before()
+	{
+	}
+
+	// tests
+	public function testSuccessGeneration()
+	{
+		$ulid = Ulid::generate();
+
+		$this->assertTrue(SymfonyUlid::isValid($ulid->value));
+	}
+}
